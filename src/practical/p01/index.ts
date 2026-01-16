f3a3dc92f12d41fbe9b9fb2c3bd46eefedc90c6b
Nithima44 <nitima7859@gmail.com>
2026-01-16 18:45:14 +0700

    index.ts

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,1 +1,61 @@
-export function getPostalAddress() {}
+import axios from 'axios';
+interface User1 {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address?: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+interface Postal {
+  id: number;
+  name: string;
+  phone: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  } | null;
+}
+export const getPostalAddress = async (): Promise<Postal[]> => {
+  try {
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    const { data } = await axios.get<User1[]>(url);
+
+    if (!data || data.length === 0) {
+      return [];
+    }
+    const format: Postal[] = data.map((user) => {
+      return {
+        id: user.id,
+        name: user.name,
+        phone: user.phone,
+        address: user.address ? user.address : null,
+      };
+    });
+    return format;
+  } catch (error) {
+    return [];
+  }
+};
+getPostalAddress().then((output) => console.log(output));
